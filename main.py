0421fb2c4d4438d21b42d20b075b5389df283041
dino596 <arthurliutw@gmail.com>
2024-03-20 14:20:31 -0700

    api

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 from flask_cors import CORS
-from scholarSearchApi import app, db
+from __init__ import app, db
 
-from scholarSearchApi.api.login import login_bp
-from scholarSearchApi.api.data import data_bp
-from scholarSearchApi.api.articles import Article_bp
+from api.ML import ml_bp
 
-from scholarSearchApi.model.login import init_login
-from scholarSearchApi.model.data import init_data
-from scholarSearchApi.model.articles import init_articles
+from model.ML import init_ml
 
-app.register_blueprint(login_bp)
-app.register_blueprint(data_bp)
-app.register_blueprint(Article_bp)
-
+app.register_blueprint(ml_bp)
 
 @app.before_first_request
 def init_db():
     with app.app_context():
         db.create_all()
-        init_login()
-        init_data()
-        init_articles()
+        init_ml()
 
 
 if __name__ == "__main__":
