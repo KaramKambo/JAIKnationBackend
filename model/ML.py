67f5b5f3c6c967869c418e90080eaf5f0d495bfa
KaramKambo <kambo.karam@gmail.com>
2024-03-21 13:00:36 -0700

    Commit for model

--- model/ML.py
+++ model/ML.py
@@ -7,31 +7,67 @@ class ML(db.Model):
     _username = Column(String, nullable=False)
     _password = Column(String, nullable=False)
 
-    def __init__(self, username, password):
-        self._username = username
-        self._password = password
+    def __init__(self, name, socialclass, age, sex, siblings, alone):
+        self._name = name
+        self._socialclass = socialclass
+        self._age = age
+        self._sex = sex
+        self._siblings = siblings
+        self._alone = alone
     
     def __repr__(self):
-        return "id='%s', username='%s', password='%s'" % (self.id, self.username, self.password)
+        return "id='%s', name='%s', socialclass='%s, age='%s, sex='%s, siblings='%s, alone='%s '" % (self.id, self.name, self.socialclass, self.age, self.sex, self.siblings, self.alone)
     
     @property
-    def username(self):
-        return self._username
+    def name(self):
+        return self._name
     
-    @username.setter
-    def username(self, value):
-        self._username = value
+    @name.setter
+    def name(self, value):
+        self._name = value
 
     @property
-    def password(self):
-        return self._password
+    def socialclass(self):
+        return self._socialclass
     
-    @password.setter
-    def password(self, value):
-        self._password = value
+    @socialclass.setter
+    def socialclass(self, value):
+        self._socialclass = value
+        
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, value):
+        self._age = value
+    
+    @property
+    def sex(self):
+        return self._sex
+    
+    @sex.setter
+    def sex(self, value):
+        self._sex = value
+    
+    @property
+    def siblings(self):
+        return self._siblings
+    
+    @siblings.setter
+    def siblings(self, value):
+        self._siblings = value
+        
+    @property
+    def alone(self):
+        return self._alone
+    
+    @alone.setter
+    def alone(self, value):
+        self._alone = value
 
     def to_dict(self):
-        return {"id": self.id, "username": self.username, "password": self.password}
+        return {"id": self.id, "name": self.name, "socialclass": self.socialclass,"age": self.age, "sex": self.sex, "siblings": self.siblings, "alone": self.alone,}
     
 def init_ml():
     db.session.commit()
